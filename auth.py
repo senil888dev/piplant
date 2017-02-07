66767a5d1c6d118bac12a831547fd79df8f948ff
Senil888 <senil888dev@gmail.com>
2017-02-07 14:56:31 -0800

    Adding Auth
    Please do NOT use these included auth keys.

---
+++ auth.py
@@ -0,0 +1,6 @@
+#The auth file. Replace 'sample' with your own keys.
+
+consumer_key='ecEJbDf3n4XjD9cFWhoNqEdLb'
+consumer_secret='QWmjhm0pfkiUv52i1e2AcSfTtDVUFF46sXey6PazVp8abYTGDo'
+key='570588269-mFtdkov0Dla9oy5LUMEHZS00opx3UgWwOuu8hgV2'
+secret='0cScOzeOF4aCpiYE2S2tihkbEpQIXZKXQgWqXoYJZioQ9'

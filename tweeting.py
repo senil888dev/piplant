69211aff53601c1a2b4a1b470be12d7cc7968744
Senil888 <senil888dev@gmail.com>
2017-02-07 17:33:55 -0800

    Everything Twitter v3.6

--- tweeting.py
+++ tweeting.py
@@ -93,8 +93,8 @@ def pollPeriod(): #this is the hard part: searching for relevant tweets, compari
 
 schedule.every().day.at("11:00").do(pollPeriod)
 schedule.every().day.at("23:00").do(startPeriod)
-schedule.every().day.at("01:25").do(startPeriod)
-schedule.every().day.at("01:27").do(pollPeriod)
+#schedule.every().day.at("01:25").do(startPeriod)
+#schedule.every().day.at("01:27").do(pollPeriod)
 
 while True:
     schedule.run_pending()
